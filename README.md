843f9de0208d84e81b58f809a56be5bcb5ca2e7d
Yehor <egor.chankoff@gmail.com>
2021-01-10 19:09:53 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # hsa-hw3
 
 Fetch the data about the last irreversible transaction on WAX and push it to the Google analytics.
+
+## Installation
+
+`pip install requests`
+
+## Run
+
+`python3 main.py`
--- utils/ga.py
+++ utils/ga.py
@@ -2,7 +2,7 @@ import requests
 
 
 def push_ts_id_old(ts_id):
-    url = f'https://www.google-analytics.com/collect?v=1&t=event&tid=your_id&cid=555&ec=wax-transactions&ea=latest-irreversible&el={ts_id}'
+    url = f'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-186989395-1&cid=555&ec=wax-transactions&ea=latest-irreversible&el={ts_id}'
     print(f'Push ts id {ts_id}')
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     return requests.get(url, headers=headers)
